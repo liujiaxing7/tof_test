7cda8432b1616d7ca8f4f83bcd0e47142a86368a
张新卫 <zhangxinwei@indemind.cn>
2023-09-11 17:37:22 +0800

    add cloudpoints denoise

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/types.hpp>
 #include "TofDepthData.h"
 #include <eigen3/Eigen/Dense>
 #include "Resolution.h"
@@ -615,7 +616,7 @@ int getBox(std::string imageFile,BoxInfo& box,std::string inputSavePath) {
 }
 
 
-void Depth2PointCloud(const cv::Mat &depth,std::vector<Eigen::Vector3d> &cloud, std::vector<Eigen::Vector3d> &selectCloudPoints,std::string fileName,BoxInfo box,std::string inputSavePath,bool usedTof,bool pointsSelected) {
+void Depth2PointCloud(const cv::Mat &depth,std::vector<Eigen::Vector3d> &cloud, std::vector<Eigen::Vector3d> &selectCloudPoints,std::string fileName,BoxInfo &box,std::string inputSavePath,bool usedTof,bool pointsSelected) {
     if (pointsSelected) {
         getBox(fileName, box, inputSavePath);
     }
@@ -912,6 +913,152 @@ int cropImage(cv::Mat image,cv::Mat &depth) {
 }
 
 
+void ConvertTof2Mat3f(cv::Mat3f &tofMat,const std::vector<Eigen::Vector3d> &cloudPoints)
+{
+    float* srcPtr = (float*)tofMat.data;
+    for (int i = 0; i < cloudPoints.size(); ++i) {
+        const Eigen::Vector3d& point = cloudPoints[i];
+        srcPtr[i * 3] = point.x();
+        srcPtr[i * 3 + 1] = point.y();
+        srcPtr[i * 3 + 2] = point.z();
+    }
+
+//    for (int i = 0; i < sizeof(tof.data) / sizeof(tof.data[0]); ++i)
+//    {
+//        srcPtr[i * 3] = tof.data[i].X;
+//        srcPtr[i * 3 + 1] = tof.data[i].Y;
+//        srcPtr[i * 3 + 2] = tof.data[i].Z;
+//    }
+}
+
+void ConvertMat3f2Tof(const cv::Mat3f &tofMat, std::vector<Eigen::Vector3d> &cloudPoints)
+{
+    float* srcPtr = (float*)tofMat.data;
+    for (int i = 0; i < cloudPoints.size(); ++i) {
+        Eigen::Vector3d& point = cloudPoints[i];
+        point.x() = srcPtr[i * 3];
+        point.y() = srcPtr[i * 3 + 1];
+        point.z() = srcPtr[i * 3 + 2];
+    }
+}
+
+void SpeckleFileter3d(cv::Mat3f& src, cv::Scalar newVal, int areaThred, float thredPtr, int axis)//cv::Mat &depth_thred_mat)
+{
+    int  h = src.rows;
+    int  w = src.cols;
+    int  pixels = h * w;
+    bool* hasLabeled = new bool[pixels];
+    int* connectX = new int[pixels];
+    int* connectY = new int[pixels];
+    int* recordLoc = new int[pixels];
+
+    float* srcPtr = (float*)src.data;
+    //float* thred_ptr = (float*)depth_thred_mat.data;
+
+    int hsize = 3 * w;
+    int loci = 0;
+    int h1 = h - 1;
+    int w1 = w - 1;
+
+    for (int k = 0; k < pixels; k++)
+    {
+        hasLabeled[k] = false;
+    }
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            int loc = loci + j;
+            int loc3Z = loc * 3 + axis;
+
+            if (!hasLabeled[loc])
+            {
+                int connectNum = 0;
+                int recordNum = 0;
+
+                connectX[connectNum] = j;
+                connectY[connectNum] = i;
+
+                hasLabeled[loc] = true;
+
+                while (connectNum >= 0 && srcPtr[loc3Z] > 0)
+                {
+                    int curX = connectX[connectNum];
+                    int curY = connectY[connectNum];
+
+                    int curLoc = curX + curY * w;
+                    int curLocZ = curLoc * 3 + axis;
+                    float curValue = srcPtr[curLocZ];
+                    float depthThred;// = fmin(0.1,fmax(0.002,thred_ptr * curValue));;//thred_ptr;//*curValue;//[curLoc];
+
+                    // 可以在外面并行算
+                    float dis2 = powf(srcPtr[curLoc * 3], 2) + powf(srcPtr[curLoc * 3 + 1], 2) + powf(srcPtr[curLoc * 3 + 2], 2);
+                    depthThred = thredPtr * dis2;
+
+                    recordLoc[recordNum] = curLocZ;
+
+                    connectNum -= 1;
+                    recordNum += 1;
+
+                    //左边
+                    if (curX > 1 && (!hasLabeled[curLoc - 1]) && fabs(srcPtr[curLocZ - 3] - curValue) < depthThred)
+                    {
+                        connectNum += 1;
+                        connectX[connectNum] = curX - 1;
+                        connectY[connectNum] = curY;
+                        hasLabeled[curLoc - 1] = true;
+                    }
+                    //右边
+                    if (curX < w1 && (!hasLabeled[curLoc + 1]) && fabs(srcPtr[curLocZ + 3] - curValue) < depthThred)
+                    {
+                        connectNum += 1;
+                        connectX[connectNum] = curX + 1;
+                        connectY[connectNum] = curY;
+                        hasLabeled[curLoc + 1] = true;
+                    }
+                    //上边
+                    if (curY > 1 && (!hasLabeled[curLoc - w]) && fabs(srcPtr[curLocZ - hsize] - curValue) < depthThred)
+                    {
+                        connectNum += 1;
+                        connectX[connectNum] = curX;
+                        connectY[connectNum] = curY - 1;
+                        hasLabeled[curLoc - w] = true;
+                    }
+                    //下边
+                    if (curY < h1 && (!hasLabeled[curLoc + w]) && fabs(srcPtr[curLocZ + hsize] - curValue) < depthThred)
+                    {
+                        connectNum += 1;
+                        connectX[connectNum] = curX;
+                        connectY[connectNum] = curY + 1;
+                        hasLabeled[curLoc + w] = true;
+                    }
+                }
+
+                //判断面积
+                if (recordNum < areaThred && srcPtr[loc3Z] > 0)
+                {
+                    for (int k = 0; k < recordNum; k++)
+                    {
+                        int cur_pos = recordLoc[k] - axis;
+                        srcPtr[cur_pos] = newVal[0];
+                        srcPtr[cur_pos + 1] = newVal[1];
+                        srcPtr[cur_pos + 2] = newVal[2];
+                    }
+                }
+            }
+        }
+
+        loci += w;
+    }
+
+
+    delete[] hasLabeled;
+    delete[] connectX;
+    delete[] connectY;
+    delete[] recordLoc;
+}
+
 
 int main(int argc, char* argv[]) {
     // 检查参数数量是否正确
@@ -959,7 +1106,11 @@ int main(int argc, char* argv[]) {
         cropImage(imageLeft,depth);
         Depth2PointCloud(depth,cloudPoints,selectCloudPoints,imageLeftPath,box,ImagePath, false, true);
 //        Depth2PointCloud(imageLeft,cloudPoints,selectCloudPoints,imageLeftPath,box,ImagePath, false, true);
-
+        // 桌子的点云去噪
+        cv::Mat3f tofMat = cv::Mat::zeros(box.h, box.w, CV_64FC3);
+        ConvertTof2Mat3f(tofMat,selectCloudPoints);
+        SpeckleFileter3d(tofMat, cv::Scalar(0,0,0),100, 0.02, 0);
+        ConvertMat3f2Tof(tofMat, selectCloudPoints);
         size_t lastSlashPos = item.imageCam0.find_last_of("/\\"); // 查找最后一个路径分隔符的位置
         std::string fileName = item.imageCam0.substr(lastSlashPos + 1);
         drawCloudTopView(ImagePath,cloudPoints,selectCloudPoints,fileName,topViewHeightMin,topViewHeightMax);
